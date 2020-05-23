769be46a7737d288b011b2119617e63eef330cc8
bertini36 <albertopouquiros@gmail.com>
2020-05-23 15:56:38 +0200

    Delete Analytics

--- blog/core/context_processors.py
+++ blog/core/context_processors.py
@@ -6,6 +6,5 @@ from django.conf import settings
 def common(request):
     return {
         'DEBUG': settings.DEBUG,
-        'RECAPTCHA_SITE_KEY': settings.RECAPTCHA_SITE_KEY,
-        'GA_MEASUREMENT_ID': settings.GA_MEASUREMENT_ID,
+        'RECAPTCHA_SITE_KEY': settings.RECAPTCHA_SITE_KEY
     }
--- blog/static/src/constants.js
+++ blog/static/src/constants.js
@@ -1,3 +1,2 @@
 export const DEBUG = window.DEBUG;
 export const RECAPTCHA_SITE_KEY = window.RECAPTCHA_SITE_KEY;
-export const GA_MEASUREMENT_ID = window.GA_MEASUREMENT_ID;
