7723efeb040b804e2e7d069e9a2c9aceb0c6be36
bertini36 <albertopouquiros@gmail.com>
2020-06-08 14:32:17 +0200

    Fix deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,7 +4,7 @@ ghpages.publish(
     '__sapper__/export',
     {
         branch: 'master',
-        repo: 'myblog.git',
+        repo: 'https://github.com/bertini36/myblog.git',
         user: {
             name: 'bertini36',
             email: 'albertopouquiros@gmail.com'
