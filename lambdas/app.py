1b169b86a62e6f4ff4f84961a7cdde5e666e396b
bertini36 <albertopouquiros@gmail.com>
2020-07-06 18:16:47 +0200

    CORS support

--- lambdas/app.py
+++ lambdas/app.py
@@ -1,12 +1,16 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 
 from exceptions import RepositoryException
 from repository import comments_repository
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/comments/<string:post_slug>', methods=['GET'])
+@cross_origin()
 def get_comments(post_slug):
     try:
         comments = comments_repository.get_comments(post_slug)
@@ -18,6 +22,7 @@ def get_comments(post_slug):
 
 
 @app.route('/comments/<string:post_slug>', methods=['POST'])
+@cross_origin()
 def add_comment(post_slug):
     try:
         comments_repository.add_comment(post_slug, request.get_json())
