94286a68081a369c20a2a6c84a9e579a3cb1164c
bertini36 <albertopouquiros@gmail.com>
2020-07-08 22:47:30 +0200

    Bindings and styles

--- src/components/Comments.svelte
+++ src/components/Comments.svelte
@@ -7,6 +7,10 @@
         return await response.json();
     })();
 
+    let name = "";
+    let email = "";
+    let comment = "";
+
     function parseDate(date_str) {
         return date_str.split(".")[0];
     }
@@ -14,7 +18,9 @@
 
 <div class="border-t-2 border-red-400 my-10"></div>
 <div>
-    <h4 class="post_section_title text-2xl text-black">Comments</h4>
+
+    <h4 class="text-2xl text-black text-center mb-10">Write a comment! 😀</h4>
+
     {#await fetch_comments}
         <figure class="flex justify-center"><img class="w-48" src="loader.gif" alt="Loader"></figure>
     {:then comments}
@@ -33,4 +39,32 @@
     {:catch error}
         <p>No comments yet</p>
     {/await}
+
+    <form class="w-full mt-10">
+        <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label for="name" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Name</label>
+                <input type="text" bind:value={name}
+                       class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+            </div>
+            <div class="w-full md:w-1/2 px-3">
+                <label for="email" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Email</label>
+                <input type="email" bind:value={email}
+                       class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+            </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+                <label for="comment" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Comment</label>
+                <textarea rows="2" bind:value={comment}
+                          class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"></textarea>
+            </div>
+        </div>
+        <div class="w-full">
+            <button class="w-full bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded">
+                Send!
+            </button>
+        </div>
+    </form>
+
 </div>
